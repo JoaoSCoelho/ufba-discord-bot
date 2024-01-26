24adcdf7ad1e3eab72eccf9d4064a73392c6c800
João Victor <jv.joaocoelho@gmail.com>
2024-01-26 16:41:07 -0300

    refactoring bathrooms command

--- src/commands/bathroom-management/bathrooms.ts
+++ src/commands/bathroom-management/bathrooms.ts
@@ -1,6 +1,6 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors, EmbedBuilder, SlashCommandBooleanOption, SlashCommandBuilder, SlashCommandUserOption } from 'discord.js';
+import { APIEmbedField, ActionRowBuilder, ButtonBuilder, ButtonStyle, Colors, EmbedBuilder, SlashCommandBooleanOption, SlashCommandBuilder, SlashCommandUserOption } from 'discord.js';
 import Command from '../../classes/Command';
-import { CampusNames, GenderNames } from '../../classes/database/Bathroom';
+import Bathroom, { CampusNames, CampusValues, GenderNames, GenderValues } from '../../classes/database/Bathroom';
 
 const data = new SlashCommandBuilder()
     .setName('bathrooms')
@@ -54,188 +54,30 @@ const data = new SlashCommandBuilder()
             .setRequired(false)
     ) as SlashCommandBuilder;
 
+
 export default new Command(
     data,
     async (interaction, client) => {
-        const filters = {
-            id: interaction.options.get('id')?.value,
-            campus: interaction.options.get('campus')?.value,
-            institute: interaction.options.get('institute')?.value,
-            floor: interaction.options.get('floor')?.value,
-            haveShower: interaction.options.get('have-shower')?.value,
-            hasHandDryer: interaction.options.get('has-hand-dryer')?.value,
-            gender: interaction.options.get('gender')?.value,
-            cabins: interaction.options.get('cabins')?.value,
-            urinals: interaction.options.get('urinals')?.value,
-            createdBy: interaction.options.get('created-by')?.value,
-            haveImage: interaction.options.get('have-image')?.value ? true : false,
-        };
-
-        const bathrooms = client.database!.bathroom.filter((bathroom) => {
-            return (filters.id ? bathroom.id === filters.id : true) &&
-                (filters.campus ? bathroom.campus === filters.campus : true) &&
-                (filters.institute ? bathroom.institute === filters.institute : true) &&
-                (filters.floor ? bathroom.floor === filters.floor : true) &&
-                (filters.haveShower ? bathroom.haveShower === filters.haveShower : true) &&
-                (filters.hasHandDryer ? bathroom.hasHandDryer === filters.hasHandDryer : true) &&
-                (filters.gender ? bathroom.gender === filters.gender : true) &&
-                (filters.cabins ? bathroom.cabins === filters.cabins : true) &&
-                (filters.urinals ? bathroom.urinals === filters.urinals : true) &&
-                (filters.createdBy ? bathroom.createdBy === filters.createdBy : true) &&
-                (filters.haveImage ? bathroom.imagesUrls.length > 0 : true);
-        });
+        const filters = getOptions();
+        const bathrooms = client.database!.bathroom.filter((bathroom) => bathroomsFilter(bathroom, filters));
 
         if (!bathrooms.size) return interaction.reply('No bathroom found');
 
         // calculates the quantity of pages (each page have 10 bathrooms)
-        const qtdPages = Math.ceil(bathrooms.size / 10);
         let currentPage = 0;
 
         // Creates a embed for each bathroom in database
-        const allEmbeds = await Promise.all(bathrooms.map(async (bathroom) => {
-            const embedAuthor = await client.users.fetch(bathroom.createdBy);
-            const bathroomFloorFormatted = bathroom.floor === 0 ?
-                'Térreo' :
-                bathroom.floor < 0 ?
-                    `${Math.abs(bathroom.floor)}º piso subsolo.` :
-                    `${bathroom.floor}º andar`;
-            const embedFields = [];
-
-
-            if (bathroom.localization)
-                embedFields.push({
-                    name: '🗺️ Onde fica?',
-                    value: bathroom.localization
-                });
-            if (bathroom.imagesUrls.length > 1)
-                embedFields.push({
-                    name: '📸 Imagens',
-                    value: bathroom.imagesUrls.reduce( // Put all images urls until 1024 characters, the ramaining will be substituted by a emoji
-                        (prev, curr) => prev.length + curr.length > 1024 - (bathroom.imagesUrls.length * 4) ? `${prev} 🖼️` : `${prev} ${curr}`,
-                        ''
-                    ),
-                });
-
-            return new EmbedBuilder({
-                title: [
-                    bathroom.gender &&
-                    (bathroom.gender === 'UNISSEX' ?
-                        '🚻' :
-                        bathroom.gender === 'MASCULINO' ?
-                            '🚹' :
-                            '🚺'),
-
-                    CampusNames[bathroom.campus],
-
-                    '-',
-
-                    bathroom.institute,
-
-                    '-',
-
-                    bathroomFloorFormatted
-                ].filter((t) => typeof t === 'string').join(' '),
-                description: [
-                    `🆔 **\`${bathroom.id}\`**`,
-
-                    bathroom.gender &&
-                    (bathroom.gender === 'UNISSEX' ?
-                        `🚾 **${GenderNames[bathroom.gender]}**` :
-                        bathroom.gender === 'FEMININO' ?
-                            `♀️ **${GenderNames[bathroom.gender]}**` :
-                            `♂️ **${GenderNames[bathroom.gender]}**`),
-
-                    `🚿 Chuveiro? **${bathroom.haveShower ? 'Sim' : 'Não'}**`,
-
-                    typeof bathroom.hasHandDryer === 'boolean' ?
-                        `🖐️ Secador de mãos? **${bathroom.hasHandDryer ? 'Sim' : 'Não'}**` :
-                        undefined,
-
-                    typeof bathroom.cabins === 'number' ?
-                        `🚽 Quantidade de cabines: **${bathroom.cabins}**` :
-                        undefined,
-
-                    (typeof bathroom.urinals === 'number' && bathroom.gender !== 'FEMININO') ?
-                        `🔫 Quantidade de mictórios: **${bathroom.urinals}**` :
-                        undefined,
-
-                    `📌 Campus: **${CampusNames[bathroom.campus]}**`,
-
-                    `🏛️ Instituto: **${bathroom.institute}**`,
-
-                    `🛗 Andar/Piso: **${bathroomFloorFormatted}**`
-                ].filter((t) => typeof t === 'string').join('\n'),
-                fields: embedFields,
-                author: {
-                    name: embedAuthor.displayName,
-                    icon_url: embedAuthor.avatarURL({ size: 64 }),
-                },
-                timestamp: bathroom.createdAt,
-                footer: {
-                    text: `Ultima atualização em ${Intl.DateTimeFormat('pt-br', { dateStyle: 'long' }).format(bathroom.updatedAt)}`
-                },
-                color: ['MASCULINO', 'FEMININO'].includes(bathroom.gender) ?
-                    bathroom.gender === 'MASCULINO' ?
-                        Colors.Blue :
-                        Colors.DarkVividPink :
-                    Colors.DarkGrey,
-                image: bathroom.mainImageUrl && { url: bathroom.mainImageUrl }
-            });
-        }));
+        const allEmbeds = await Promise.all(bathrooms.map((bathroom) => embedFactory(bathroom)));
 
 
         // Inputs on "paginatedEmbeds" constant all embeds divided in arrays: [[10_embeds], [10_embeds], ..., [remaining_embeds]]
-        const paginatedEmbeds = [];
-        for (let i = 0; i < qtdPages; i++) {
-            paginatedEmbeds.push(allEmbeds.slice(0 + (i * 10), 10 + (i * 10)));
-        }
-
-        // Creates a new ButtonComponent, default disabled to show the current page that the user is navigating
-        const createCurrentPageButton = () => new ButtonBuilder()
-            .setCustomId('current-page')
-            .setDisabled()
-            .setStyle(ButtonStyle.Secondary)
-            .setLabel(`${currentPage + 1} / ${paginatedEmbeds.length}`);
-
-        const createBackward10Button = () => new ButtonBuilder()
-            .setCustomId('backward-10')
-            .setLabel('10 <<')
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage + 1 <= 2 ? true : false);
-
-        const createBackwardButton = () => new ButtonBuilder()
-            .setCustomId('backward')
-            .setLabel('1 <<')
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage + 1 === 1 ? true : false);
-
-        const createForwardButton = () => new ButtonBuilder()
-            .setCustomId('forward')
-            .setLabel('>> 1')
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage + 1 === paginatedEmbeds.length);
-
-        const createForward10Button = () => new ButtonBuilder()
-            .setCustomId('forward-10')
-            .setLabel('>> 10')
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(currentPage + 1 >= paginatedEmbeds.length - 1);
-
-        const rowComponents = () => new ActionRowBuilder<ButtonBuilder>()
-            .setComponents(
-                createBackward10Button(),
-                createBackwardButton(),
-                createCurrentPageButton(),
-                createForwardButton(),
-                createForward10Button()
-            );
-
+        const paginatedEmbeds = paginatedEmbedsFactory(allEmbeds);
+        
 
         const response = await interaction.reply({
             content: `${bathrooms.size} resultados encontrados`,
-            embeds:
-                paginatedEmbeds[currentPage],
-            components: paginatedEmbeds.length > 1 ? [rowComponents()] : undefined
+            embeds: paginatedEmbeds[currentPage],
+            components: paginatedEmbeds.length > 1 ? [rowComponentsFactory()] : undefined
         });
 
 
@@ -254,8 +96,189 @@ export default new Command(
 
             await i.update({
                 embeds: paginatedEmbeds[currentPage],
-                components: [rowComponents()]
+                components: [rowComponentsFactory()]
             });
         });
+
+
+
+
+
+
+        function getOptions() {
+            return {
+                id: interaction.options.get('id')?.value as string | undefined,
+                campus: interaction.options.get('campus')?.value as CampusValues | undefined,
+                institute: interaction.options.get('institute')?.value as string | undefined,
+                floor: interaction.options.get('floor')?.value as number | undefined,
+                haveShower: interaction.options.get('have-shower')?.value as boolean | undefined,
+                hasHandDryer: interaction.options.get('has-hand-dryer')?.value as boolean | undefined,
+                gender: interaction.options.get('gender')?.value as GenderValues | undefined,
+                cabins: interaction.options.get('cabins')?.value as number | undefined,
+                urinals: interaction.options.get('urinals')?.value as number | undefined,
+                createdBy: interaction.options.get('created-by')?.value as string | undefined,
+                haveImage: interaction.options.get('have-image')?.value as boolean | undefined,
+            };
+        }
+
+        function bathroomsFilter(bathroom: Bathroom, filters: ReturnType<typeof getOptions>) {
+            return (filters.id ? bathroom.id === filters.id : true) &&
+                (filters.campus ? bathroom.campus === filters.campus : true) &&
+                (filters.institute ? bathroom.institute === filters.institute : true) &&
+                (filters.floor ? bathroom.floor === filters.floor : true) &&
+                (filters.haveShower ? bathroom.haveShower === filters.haveShower : true) &&
+                (filters.hasHandDryer ? bathroom.hasHandDryer === filters.hasHandDryer : true) &&
+                (filters.gender ? bathroom.gender === filters.gender : true) &&
+                (filters.cabins ? bathroom.cabins === filters.cabins : true) &&
+                (filters.urinals ? bathroom.urinals === filters.urinals : true) &&
+                (filters.createdBy ? bathroom.createdBy === filters.createdBy : true) &&
+                (filters.haveImage ? bathroom.imagesUrls.length > 0 : true);
+
+        }
+
+        async function embedFactory(bathroom: Bathroom) {
+            const embedAuthor = await client.users.fetch(bathroom.createdBy);
+            const bathroomFloorFormatted = formatFloor();
+            const lastUpdateFormatted = Intl.DateTimeFormat('pt-br', { dateStyle: 'long' }).format(bathroom.updatedAt);
+
+            return new EmbedBuilder({
+                title: `${CampusNames[bathroom.campus]} - ${bathroom.institute} - ${bathroomFloorFormatted}`,
+                description: descriptionFactory(bathroomFloorFormatted),
+                fields: fieldsFactory(),
+                author: { name: `Criado por **${embedAuthor.displayName}**`, icon_url: embedAuthor.avatarURL({ size: 64 }) },
+                timestamp: bathroom.createdAt,
+                footer: { text: `Ultima atualização em ${lastUpdateFormatted}` },
+                color: colorFactory(),
+                image: bathroom.mainImageUrl && { url: bathroom.mainImageUrl }
+            });
+
+
+
+            function formatFloor() {
+                if (bathroom.floor === 0) return 'Térreo';
+                else if (bathroom.floor < 0) return `${Math.abs(bathroom.floor)}º piso subsolo.`;
+                else return `${bathroom.floor}º andar`;
+            }
+
+            function descriptionFactory(bathroomFloorFormatted: string) {
+                const id = `🆔 **\`${bathroom.id}\`**`;
+                const gender = genderFactory();
+                const haveShower = `🚿 Chuveiro? **${bathroom.haveShower ? 'Sim' : 'Não'}**`;
+                const hasHandDryer = typeof bathroom.hasHandDryer === 'boolean' ?
+                    `🖐️ Secador de mãos? **${bathroom.hasHandDryer ? 'Sim' : 'Não'}**` :
+                    undefined;
+                const cabins = typeof bathroom.cabins === 'number' ?
+                    `🚽 Quantidade de cabines: **${bathroom.cabins}**` :
+                    undefined;
+                const urinals = (typeof bathroom.urinals === 'number' && bathroom.gender !== 'FEMININO') ?
+                    `🔫 Quantidade de mictórios: **${bathroom.urinals}**` :
+                    undefined;
+                const campus = `📌 Campus: **${CampusNames[bathroom.campus]}**`;
+                const institute = `🏛️ Instituto: **${bathroom.institute}**`;
+                const floor = `🛗 Andar/Piso: **${bathroomFloorFormatted}**`;
+
+
+                return [id, gender, haveShower, hasHandDryer, cabins, urinals, campus, institute, floor]
+                    .filter((t) => typeof t === 'string').join('\n');
+
+
+
+                function genderFactory(): string | undefined {
+                    if (bathroom.gender === 'UNISSEX') return `🚾 **${GenderNames[bathroom.gender]}**`;
+                    else if (bathroom.gender === 'FEMININO') return `♀️ **${GenderNames[bathroom.gender]}**`;
+                    else if (bathroom.gender === 'MASCULINO') return `♂️ **${GenderNames[bathroom.gender]}**`;
+                }
+            }
+
+            function fieldsFactory() {
+                const fields: APIEmbedField[] = [];
+                if (bathroom.localization)
+                    fields.push({
+                        name: '🗺️ Onde fica?',
+                        value: bathroom.localization
+                    });
+                if (bathroom.imagesUrls.length > 1)
+                    fields.push({
+                        name: '📸 Imagens',
+                        value: bathroom.imagesUrls.reduce( // Put all images urls until 1024 characters, the ramaining will be substituted by a emoji
+                            (prev, curr) => prev.length + curr.length > 1024 - (bathroom.imagesUrls.length * 4) ? `${prev} 🖼️` : `${prev} ${curr}`,
+                            ''
+                        ),
+                    });
+
+                return fields;
+            }
+
+            function colorFactory() {
+                if (bathroom.gender === 'MASCULINO') return Colors.Blue;
+                else if (bathroom.gender === 'FEMININO') return Colors.LuminousVividPink;
+                else return Colors.DarkGrey;
+            }
+        }
+
+        
+        function rowComponentsFactory() {
+            
+            return new ActionRowBuilder<ButtonBuilder>()
+                .setComponents(
+                    backward10ButtonFactory(),
+                    backwardButtonFactory(),
+                    currentPageButtonFactory(),
+                    forwardButtonFactory(),
+                    forward10ButtonFactory()
+                );
+
+            // Creates a new ButtonComponent, default disabled to show the current page that the user is navigating
+
+            function currentPageButtonFactory() {
+                return new ButtonBuilder()
+                    .setCustomId('current-page')
+                    .setDisabled()
+                    .setStyle(ButtonStyle.Secondary)
+                    .setLabel(`${currentPage + 1} / ${paginatedEmbeds.length}`);
+            }
+        
+            function backward10ButtonFactory() {
+                return new ButtonBuilder()
+                    .setCustomId('backward-10')
+                    .setLabel('10 <<')
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(currentPage + 1 <= 2 ? true : false);
+            }
+        
+            function backwardButtonFactory() {
+                return new ButtonBuilder()
+                    .setCustomId('backward')
+                    .setLabel('1 <<')
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(currentPage + 1 === 1 ? true : false);
+            }
+        
+            function forwardButtonFactory() {
+                return new ButtonBuilder()
+                    .setCustomId('forward')
+                    .setLabel('>> 1')
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(currentPage + 1 === paginatedEmbeds.length);
+            }
+        
+            function forward10ButtonFactory() {
+                return new ButtonBuilder()
+                    .setCustomId('forward-10')
+                    .setLabel('>> 10')
+                    .setStyle(ButtonStyle.Secondary)
+                    .setDisabled(currentPage + 1 >= paginatedEmbeds.length - 1);
+            }
+        }
+
+        function paginatedEmbedsFactory(embeds: EmbedBuilder[]) {
+            const paginated: EmbedBuilder[][] = [];
+            const qtdPages = Math.ceil(bathrooms.size / 10);
+
+            for (let i = 0; i < qtdPages; i++) {
+                paginated.push(embeds.slice(0 + (i * 10), 10 + (i * 10)));
+            }
+            return paginated;
+        }
     }
 );
