53aaca3b453b432720677072665276d07e85b9da
João Victor <jv.joaocoelho@gmail.com>
2024-02-29 08:37:32 -0300

    Update score.ts

--- src/commands/general/score.ts
+++ src/commands/general/score.ts
@@ -1,19 +1,31 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, SlashCommandUserOption } from 'discord.js';
 import Command from '../../classes/Command';
 import { levels } from '../../score-system';
 
 export default new Command(
     new SlashCommandBuilder()
         .setName('score')
-        .setDescription('Mostra o seu escore de interação neste servidor e o seu nível atual.'),
+        .setDescription('Mostra o seu escore de interação neste servidor e o seu nível atual.')
+        .addUserOption(
+            new SlashCommandUserOption()
+                .setName('membro')
+                .setDescription('Você pode buscar o score de um membro específico.')
+        ) as SlashCommandBuilder,
 
     async (interaction, client) => {
-        const member = client.database!.member.find((member) => member.discordId === interaction.user.id && member.discordGuildId === interaction.guildId);
+        if (!interaction.inGuild()) return;
 
-        if (!member) return interaction.reply('Você não possui pontuação neste servidor!');
+        const targetMemberId = interaction.options.get('membro')?.user!.id ?? interaction.user.id;
+        const guildMember = await client.guilds.cache.get(interaction.guildId)!.members.fetch(targetMemberId);
+
+        const member = client.database!.member.find((member) => {
+            return  member.discordId === targetMemberId && member.discordGuildId === interaction.guildId;
+        });
+
+        if (!member) return interaction.reply(`${guildMember} não possui pontuação neste servidor!`);
 
         const levelIndex = levels.findIndex(({ targetScore }) => targetScore > member.score);
 
-        interaction.reply(`Você tem \`${member.score}\` pontos e seu nível atual é **Nível ${levelIndex}**`);
+        interaction.reply(`${guildMember} tem \`${member.score}\` pontos e seu nível atual é **Nível ${levelIndex}**`);
     }
 );
