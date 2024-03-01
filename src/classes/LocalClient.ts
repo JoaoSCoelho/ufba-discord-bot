4f91a496b05f4b0e9771599006f790e546f75afe
João Victor <jv.joaocoelho@gmail.com>
2024-03-01 09:32:53 -0300

    Update LocalClient.ts

--- src/classes/LocalClient.ts
+++ src/classes/LocalClient.ts
@@ -1,12 +1,21 @@
-import { Client, Collection } from 'discord.js';
+import { Client, ClientOptions, Collection } from 'discord.js';
 import Command from './Command';
 import Database from '../database/Database';
 import AdminCommand from './AdminCommand';
+import { log } from '..';
 
 export default class LocalClient extends Client {
+    /** `PT`: Array de IDs de usuários Discord que possuem poder de `ADMIN` no bot */
     public admins = process.env.BOT_ADMINS!.split(',');
     public commands = new Collection<string, Command>();
     public adminCommands = new Collection<string, AdminCommand>();
     public database?: Database;
     public prefix: string = process.env.PREFIX || '_';
+
+
+    constructor(options: ClientOptions) {
+        super(options);
+
+        log.successh('Client instanciado');
+    }
 }
