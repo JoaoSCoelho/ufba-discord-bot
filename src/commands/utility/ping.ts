aa4b268d9059960bdd6a39ac89060c19ebcf85c9
João Victor <jv.joaocoelho@gmail.com>
2024-03-02 21:58:58 -0300

    Update ping.ts

--- src/commands/utility/ping.ts
+++ src/commands/utility/ping.ts
@@ -3,8 +3,12 @@ import Command from '../../classes/Command';
 import prettyTime from 'pretty-time';
 
 export default new Command(
-    new SlashCommandBuilder().setName('ping').setDescription('Responde com Pong!'),
+    new SlashCommandBuilder().setName('ping').setDescription('Informa o tempo ativo do bot e a latência do WebSocket!'),
     async (interaction, client) => {
         await interaction.reply(`Pong!${client.uptime ? ` Bot ativo a **${prettyTime(client.uptime * 1_000_000, 'm')}**.` : ''} Latência WebSocket **${prettyTime(client.ws.ping * 1_000_000, 'ms')}**.`);
-    } 
+    },
+    {
+        aliases: ['latencia', 'latency', 'uptime', 'tempo-ativo'],
+        howToUse: 'Digite /ping no chat e selecione o comando na lista',
+    }
 );
