39803623b229027e58dda1bf02c984cd47fce621
João Victor <jv.joaocoelho@gmail.com>
2024-03-08 15:11:27 -0300

    remove aliases

--- src/admin-commands/deploy.ts
+++ src/admin-commands/deploy.ts
@@ -8,12 +8,5 @@ async (_message, client) => {
     // Starts this var with commandsJson by name only
     const commandsJson = client.commands.map((command) => command.data.toJSON());
 
-    // Push to `commandsJson` others commandsJson by aliases
-    client.commands.forEach((command) => {
-        command.documentation?.aliases?.map((alias) => {
-            commandsJson.push(({ ...command.data.toJSON(), name: alias }));
-        });
-    });
-
     deployCommands(commandsJson);
 });
