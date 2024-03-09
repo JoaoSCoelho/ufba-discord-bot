258a8b4277f2f64bdd5c26fb4964981743dc35fc
João Victor <jv.joaocoelho@gmail.com>
2024-03-09 10:24:44 -0300

    Update configEnv.ts

--- src/configEnv.ts
+++ src/configEnv.ts
@@ -1,3 +1,6 @@
 import { config } from 'dotenv';
+import { log } from './classes/LogSystem';
 
+log.loadingh('Setando variáveis de ambiente de #(.env)# para #(process.env)#');
 config();
+log.successh('Variáveis de ambiente setadas');
