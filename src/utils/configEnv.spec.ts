0c994a97c5a27ac4bd0baa0610e0de0261db52f8
João Victor <jv.joaocoelho@gmail.com>
2025-01-14 22:08:23 -0300

    create test for configEnv

---
+++ src/utils/configEnv.spec.ts
@@ -0,0 +1,37 @@
+import { config } from 'dotenv';
+import { configEnv } from './configEnv';
+import { log } from '../classes/LogSystem';
+
+jest.mock('../classes/LogSystem', () => ({
+    log: {
+        loadingh: jest.fn(),
+        successh: jest.fn(),
+        error: jest.fn(),
+    },
+}));
+jest.mock('dotenv', () => ({
+    config: jest.fn(),
+}));
+
+describe('configEnv', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should call config() and log.successh() when no error occurs', () => {
+        configEnv();
+        expect(config).toHaveBeenCalled();
+        expect(log.successh).toHaveBeenCalled();
+        expect(log.error).not.toHaveBeenCalled();
+    });
+
+    it('should call log.error() when an error occurs', () => {
+        (config as jest.Mock).mockImplementationOnce(() => {
+            throw new Error('Test Error');
+        });
+        configEnv();
+        expect(config).toHaveBeenCalled();
+        expect(log.successh).not.toHaveBeenCalled();
+        expect(log.error).toHaveBeenCalled();
+    });
+});
--- src/utils/configEnv.ts
+++ src/utils/configEnv.ts
@@ -1,15 +1,16 @@
 import { config } from 'dotenv';
 import { log } from '../classes/LogSystem';
 
-function configEnv() {
+export function configEnv() {
     try {
         log.loadingh('Setando variáveis de ambiente de #(.env)# para #(process.env)#');
         config();
         log.successh('Variáveis de ambiente setadas');
     } catch (error) {
-        log.errorh('Erro ao setar variáveis de ambiente de #(.env)# para #(process.env)#\n',
+        log.error('Erro ao setar variáveis de ambiente de #(.env)# para #(process.env)#\n',
             '#(Erro)#:', error);
     }
 }
 
+// Config by default
 configEnv();
