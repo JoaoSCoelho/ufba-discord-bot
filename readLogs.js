3de6028ae7c3ae45277ba59a0b0517c001cdda06
João Victor <jv.joaocoelho@gmail.com>
2025-02-27 21:08:46 -0300

    Update readLogs.js

--- readLogs.js
+++ readLogs.js
@@ -1,1 +1,1 @@
-require('fs').readFileSync('log.ansi', { encoding: 'utf8' }).split(/\n|\r/g).forEach((logLine) => console.log(logLine));
+require('fs').readFileSync(process.argv[2] || 'log.ansi', { encoding: 'utf8' }).split(/\n|\r/g).forEach((logLine) => console.log(logLine));
