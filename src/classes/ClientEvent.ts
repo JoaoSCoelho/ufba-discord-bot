78cb04e0c3fc9ea1c49f85dea1d5bd6b95c5330f
João Victor <jv.joaocoelho@gmail.com>
2025-03-07 16:53:13 -0300

    document clientEvent

--- src/classes/ClientEvent.ts
+++ src/classes/ClientEvent.ts
@@ -1,9 +1,14 @@
 import { Awaitable, ClientEvents, } from 'discord.js';
 
 export default class ClientEvent<Event extends keyof ClientEvents> {
+    /**
+     * @param eventName - The name of the event to listen for.
+     * @param listener - The function to be called when the event is emitted.
+     * @param once - `true` if the listener should be called only once.
+     */
     constructor(
         public eventName: Event,
         public listener: (...args: ClientEvents[Event]) => Awaitable<void>,
         public once?: boolean,
-    ) {}
+    ) { }
 }
