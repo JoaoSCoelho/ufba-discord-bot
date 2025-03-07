746d7afd29b64ed5c3c318c99a6495be35fbdcf1
João Victor <jv.joaocoelho@gmail.com>
2025-03-07 15:45:49 -0300

    documents isObject

--- src/utils/isObject.ts
+++ src/utils/isObject.ts
@@ -1,3 +1,8 @@
+/**
+ * Checks if a value is an object.
+ * @param testValue The value to check.
+ * @returns `true` if the value is an object, `false` otherwise.
+ */
 export default function isObject(testValue: unknown) {
     return testValue !== null && typeof testValue === 'object' && !Array.isArray(testValue);
 }
