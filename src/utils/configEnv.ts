02eb4176b6c6ecfa87a61c9c074b906524ccece2
João Victor <jv.joaocoelho@gmail.com>
2025-03-07 15:55:45 -0300

    create configenv documentation

--- src/utils/configEnv.ts
+++ src/utils/configEnv.ts
@@ -4,6 +4,7 @@ import { config } from 'dotenv';
 import { log } from '../classes/LogSystem';
 import BaseError from '../Errors/BaseError';
 
+/** Sets in the `process.env` the variables writed in file `.env`. */
 export function configEnv() {
     try {
         log.loadingh('Setando variáveis de ambiente de #(.env)# para #(process.env)#.');
